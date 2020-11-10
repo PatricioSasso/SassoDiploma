243b5c0ae20357069052517e69fdc896810424a4
PatricioSasso <patonsasso@gmail.com>
2020-11-10 19:39:53 -0300

    Implemented a Test Project and added some example tests

---
+++ SassoCampo/Test/ControlDeAcceso_LogIn_Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class ControlDeAcceso_LogIn_Test
+    {
+        [TestMethod]
+        public void UserPatoAndPassxAgunimon_ShouldReturnTrue()
+        {
+            ControlDeAccesoGestor control = new ControlDeAccesoGestor();
+            var userName = "Pato";
+            var password = control.GetHash("xAgunimon");
+            var result = control.LogIn(userName, password);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TestExecption()
+        {
+            ControlDeAccesoGestor control = new ControlDeAccesoGestor();
+            var userName = "Pato";
+            string password = null;
+            var result = control.LogIn(userName, password);
+        }
+    }
+}
