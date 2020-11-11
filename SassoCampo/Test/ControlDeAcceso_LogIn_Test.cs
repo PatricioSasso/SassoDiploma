a69268924b12f5fecd231ed01c9266541cf7a4a7
PatricioSasso <patonsasso@gmail.com>
2020-11-11 10:43:07 -0300

    Added more tests

--- SassoCampo/Test/ControlDeAcceso_LogIn_Test.cs
+++ SassoCampo/Test/ControlDeAcceso_LogIn_Test.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.Sql;
+using Service;
+using BLL;
+using GUI;
+using System.Data.SqlClient;
 
 namespace Test
 {
@@ -7,7 +12,7 @@ namespace Test
     public class ControlDeAcceso_LogIn_Test
     {
         [TestMethod]
-        public void UserPatoAndPassxAgunimon_ShouldReturnTrue()
+        public void BaseLogin()
         {
             ControlDeAccesoGestor control = new ControlDeAccesoGestor();
             var userName = "Pato";
@@ -17,12 +22,40 @@ namespace Test
         }
 
         [TestMethod]
-        public void TestExecption()
+        public void EmptyUser()
         {
             ControlDeAccesoGestor control = new ControlDeAccesoGestor();
-            var userName = "Pato";
-            string password = null;
+            var userName = string.Empty;
+            string password = string.Empty;
             var result = control.LogIn(userName, password);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void FilterTest()
+        {
+            BitacoraGestor bitacoraGestor = new BitacoraGestor();
+            var expectedResult = bitacoraGestor.GetAll();
+            var result = bitacoraGestor.BuscarFecha(string.Empty);
+            CollectionAssert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(SqlException))]
+        public void ProductoTest()
+        {
+            ProductoGestor productoGestor = new ProductoGestor();
+            Producto productoEsperado = new Producto();
+            productoGestor.Alta(productoEsperado);
+        }
+
+        [TestMethod]
+        public void UserFilterTest()
+        {
+            BitacoraGestor bitacoraGestor = new BitacoraGestor();
+            int expectedResult = 0;
+            var result = bitacoraGestor.BuscarFecha("x");
+            Assert.AreEqual(expectedResult, result);
         }
     }
 }
